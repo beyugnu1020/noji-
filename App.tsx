017e06bc7cb267eb7f6a052d724b1a2d611bf36c
beyugnu1020 <beyungu1386@gmail.com>
2025-03-29 06:31:21 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <h1>노지캠 - 캠핑 친구를 만들어요!</h1>;
+}
---
+++ firebaseConfig.ts
@@ -0,0 +1,5 @@
+export const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+};
